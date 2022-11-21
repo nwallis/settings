79dadc9b36436975a570eba6afe827b94bacb324
Nathan Wallis <natewallis@gmail.com>
2022-11-22 06:20:49 +1100

    adds git aliases to deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+# Copy all vim to the home folder
 rsync -avh .vim* ~/
+
+# Git Aliases
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+git config --global alias.a add
+git config --global alias.p push
